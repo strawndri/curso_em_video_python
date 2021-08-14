6f0804eabd1a484c4c048eaf29a94a00897cd3a0
Andrieli Luci <andrieliluci@gmail.com>
2021-08-14 18:23:03 -0300

    Aula 21 - Ex103 v2

---
+++ Mundo 03/ex103-2.py
@@ -0,0 +1,22 @@
+# Aula 21
+
+# Faça um programa que tenha uma função chamada ficha(), que receba dois parâmetros opcionais:
+# o nome de um jogador e quantos gols ele marcou. O programa deverá ser capaz de motrar a ficha
+# do jogador, mesmo que algum dado não tenha sico informado corretamente.
+
+def ficha(jogador='<desconhecido>', gols=0):
+    return print(f'O jogador {jogador} fez {gols} gol(s)!')
+
+
+nome = input('Nome do Jogador: ')
+quantidade = input('Quantidade de gols: ')
+
+if (quantidade.isnumeric()):
+    quantidade = int(quantidade)
+else:
+    quantidade = 0
+
+if (nome == ''):
+    ficha(gols=quantidade)
+else:
+    ficha(nome, quantidade)
