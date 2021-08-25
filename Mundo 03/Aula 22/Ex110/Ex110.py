e18d393cd5f6c47737ff509a97ecbbf945f9c5ae
Andrieli Luci <andrieliluci@gmail.com>
2021-08-25 18:31:44 -0300

    Aula 22 - Ex110

---
+++ Mundo 03/Aula 22/Ex110/Ex110.py
@@ -0,0 +1,13 @@
+# Aula 22
+
+
+# Adicione ao módulo moeda.py criado nos desafios anteriores, uma função chamada resumo(),
+# que mostre na tela algumas iformações geradas pelas funções que já temos no módulo criado
+# até aqui.
+
+
+import moeda as md
+
+valor = float(input('Preço do produto, em reais: R$ '))
+md.resumo(valor, 10, 20)
+
