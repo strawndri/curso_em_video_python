066bd29fd5673bd6dd12cf3075554c413ef7f699
Andrieli Luci <andrieliluci@gmail.com>
2021-08-25 18:17:20 -0300

    Aula 22 - Ex108

---
+++ Mundo 03/Aula 22/Ex108/moeda.py
@@ -0,0 +1,29 @@
+def moeda(valor):
+    moeda = f'R$ {valor:.2f}'
+    return moeda
+
+
+def aumentar(preco):
+    novo_preco = preco + (preco * 0.10)
+
+    return novo_preco
+
+
+def diminuir(preco):
+    novo_preco = preco - (preco * 0.10)
+
+    return novo_preco
+
+
+def dobro(preco):
+    novo_preco = preco * 2
+
+    return novo_preco
+
+
+def metade(preco):
+    novo_preco = preco /2
+
+    return novo_preco
+
+
